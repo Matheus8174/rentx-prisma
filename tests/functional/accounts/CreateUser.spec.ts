052aa746f1e573722b435e7a23498596c19dfc54
Matheus Araújo <souzamatheus8174@gmail.com>
2022-09-29 14:05:34 -0300

    refactor: change data type for create user dto

--- tests/functional/accounts/CreateUser.spec.ts
+++ tests/functional/accounts/CreateUser.spec.ts
@@ -1,11 +1,11 @@
 import { faker } from '@faker-js/faker';
-import { RequestBody } from '@modules/accounts/useCases/createUser/CreateUserController';
+import ICreateUserDTO from '@modules/accounts/dtos/ICreateUserDTO';
 
 const prefix = process.env.PREFIX_ROUTES;
 
 describe('#POST /users', () => {
   it('should be able to create a new user', async () => {
-    const data: RequestBody = {
+    const data: ICreateUserDTO = {
       name: faker.internet.userName(),
       email: faker.internet.email(),
       driver_license: faker.lorem.text(),
